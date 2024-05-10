e31fb37b0d394364b71228a1fce576dbb167aee4
heidih <heidi.hati@sulake.com>
2024-05-10 20:56:15 +0300

    Clean up draggable class

--- src/component/DraggableItem.tsx
+++ src/component/DraggableItem.tsx
@@ -36,7 +36,7 @@ const DraggableItem = ({ id, item }: Props) => {
   return (
     <div ref={setNodeRef} style={style} className={`border h-full cursor-grab text-xs active:cursor-grabbing ${ isInChain() && 'bg-accent'}`} {...listeners} {...attributes}>
       <div className="avatar h-full w-full">
-        <div className={`${item.isInsideBubble ? 'rounded-full ring ring-neutral ring-inset ring-offset-2' : ''} ${ item.visibility === 'hidden' ? 'text-secondary bg-gray-950/50' : 'text-primary-content'}`}>
+        <div className={`${item.isInsideBubble && 'rounded-full ring ring-neutral ring-inset ring-offset-2'} ${ item.visibility === 'hidden' ? 'text-secondary bg-gray-950/50' : 'text-primary-content'}`}>
           <div className="flex h-full w-full justify-center text-center">
             <div className="p-2 w-full break-words my-auto">
               <div>{ item.chainId.replace(/_/g, ' ').replace(/([a-z])([A-Z])/g, '$1 $2') }</div>
