4dda5ad667a845826c111b083a443f27f6bccf4c
heidih <heidi.hati@sulake.com>
2024-05-10 20:52:05 +0300

    Highlight items in the same chain

--- src/component/DraggableItem.tsx
+++ src/component/DraggableItem.tsx
@@ -1,8 +1,9 @@
 
 import { useDraggable } from '@dnd-kit/core'
 import { CSS } from '@dnd-kit/utilities'
-import React from 'react'
+import React, { useContext } from 'react'
 
+import { SelectedItemContext } from '../App'
 import { ItemData } from '../model/boardData'
 
 type Props = {
@@ -10,7 +11,22 @@ type Props = {
   item: ItemData
 }
 const DraggableItem = ({ id, item }: Props) => {
+
+  const selectedItem = useContext(SelectedItemContext)
+
   const { attributes, listeners, setNodeRef, transform } = useDraggable({ id })
+
+  const isInChain = () => {
+    if (!selectedItem) {
+      return false
+    }
+
+    if (selectedItem.id === id) {
+      return false
+    }
+
+    return selectedItem.data.chainId === item.chainId
+  }
 
   const style = {
     // Outputs `translate3d(x, y, 0)`
@@ -18,7 +34,7 @@ const DraggableItem = ({ id, item }: Props) => {
   }
 
   return (
-    <div ref={setNodeRef} style={style} className="border h-full cursor-grab text-xs active:cursor-grabbing" {...listeners} {...attributes}>
+    <div ref={setNodeRef} style={style} className={`border h-full cursor-grab text-xs active:cursor-grabbing ${ isInChain() && 'bg-accent'}`} {...listeners} {...attributes}>
       <div className="avatar h-full w-full">
         <div className={`${item.isInsideBubble ? 'rounded-full ring ring-neutral ring-inset ring-offset-2' : ''} ${ item.visibility === 'hidden' ? 'text-secondary bg-gray-950/50' : 'text-primary-content'}`}>
           <div className="flex h-full w-full justify-center text-center">
